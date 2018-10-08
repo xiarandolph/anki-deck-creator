a4baa1d2fdc2fe99b0876161dc81a7d6e8b1b75f
Randolph Xia <xia.randolph.227@gmail.com>
2018-10-07 20:09:50 -0400

    Update main program file with commands create_note and write_out

--- AnkiDeckCreator/deckcreator.py
+++ AnkiDeckCreator/deckcreator.py
@@ -1,14 +1,11 @@
 """
-Module defines DeckCreator class
-
-TODO:
-    * create_notes(): Create card pairings for data
-    * card pairing options: pair every column to eachother
+Module defines DeckCreator class and some basic models
 """
 
 import genanki
 import random   # generating UUID
 
+## CONSTS
 card_css = '''.card {
     font-family: arial;
     font-size: 20px;
@@ -17,39 +14,76 @@ card_css = '''.card {
     background-color: white;
 }'''
 
-class DeckCreator:
-    fill_in_model = genanki.Model(
-        1835728501,     # MODEL ID
-        'Short Answer Model',
-        fields=[
-            {'name': 'Question'},
-            {'name': 'Answer'}
-        ],
-        templates=[
-            {
-                'name': 'Card 1',
-                'qfmt': '{{Question}}\n{{type:Answer}}',
-                'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}'
-            },
-        ],
-        css=card_css,
-    )
+basic_model = genanki.Model(
+    1936820195,
+    'Basic Model',
+    fields=[
+        {'name': 'Front'},
+        {'name': 'Back'}
+    ],
+    templates=[
+        {
+            'name': 'Card 1',
+            'qfmt': '{{Front}}',
+            'afmt': '{{FrontSide}}<hr id="back">{{Back}}'
+        },
+    ],
+    css=card_css,
+)
+
+short_ans_model = genanki.Model(
+    1835728501,     # MODEL ID
+    'Short Answer Model',
+    fields=[
+        {'name': 'Front'},
+        {'name': 'Back'}
+    ],
+    templates=[
+        {
+            'name': 'Card 1',
+            'qfmt': '{{Front}}\n{{type:Back}}',
+            'afmt': '{{FrontSide}}<hr id="back">{{Back}}'
+        },
+    ],
+    css=card_css,
+)
+
+def custom_model(self, id, name, fields, templates, css):
+    """
+    @param  id          model id
+    @param  name        model name
+    @param  fields      list of dictionaries {'name': 'field name'}
+    @param  templates   list of card templates
+    @param  css         custom css string .card {}
+    """
     
+    return genanki.Model(
+        id,
+        name,
+        fields=fields,
+        templates=templates,
+        css=css
+    )
+
+class DeckCreator:
     def __init__(self, deck_name, deck_id = random.randrange(1 << 30, 1 << 31)):
         self.deck = genanki.Deck(deck_id, deck_name)
     
-    def create_notes(self, data):
-        notes = []
-        test_note = genanki.Note(
-            model=fill_in_model,
-            fields=['Question', 'Answer']
+    def create_note(self, data, model):
+        """
+        @param  data    two-tuple containing front and back fields (front, back)
+        @param  model   basic_model or short_ans_model
+        """
+        print(data)
+        print(model)
+        note = genanki.Note(
+            model=model,
+            fields=[str(data[0]), str(data[1])]
         )
-        notes.append(test_note)
-        return notes
-    
-    def populate_deck(self, data):
-        for note in create_notes(data):
-            self.deck.add_note(note)
+        self.deck.add_note(note)
     
     def write_to_file(self, filename):
+        extension = filename.split('.')
+        if (len(extension) != 2 or extension[1] != 'apkg'):
+            print("Error: invalid filename, expected .apkg")
         genanki.Package(self.deck).write_to_file(filename)
--- AnkiDeckCreator/main.py
+++ AnkiDeckCreator/main.py
@@ -5,16 +5,26 @@ Example:
     $ python3 main.py input.xlsx
 
 TODO:
-    * Parse data and create deck and .apkg file
+    * Make usage more intuitive
 """
 
 import sys
 from parser import Parser
-from deckcreator import DeckCreator
+from deckcreator import DeckCreator, basic_model, short_ans_model
 
 def print_usage():
     # Print command line arguments
     print("Usage: main.py <filename>")
+
+def print_commands():
+    print("\nAvailable commands:")
+    print("\tmake_notes <front column> <back column> <card model>")
+    print("\twrite_out <filename>")
+    print("\thelp")
+    print("\tquit")
+    print("\nAvailable models:")
+    print("\tshort_ans_model")
+    print("\tbasic_model")
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -27,4 +37,60 @@ if __name__ == '__main__':
         print("Error: Could not open file", sys.argv[1])
         exit(1)
     
-    dc = DeckCreator("Chinese")
+    print("Data found:")
+    for title in fp.get_titles():
+        print("{:10s}:".format(title), len(fp.data[title]), "items")
+    
+    deck_name = input("\nEnter a deck name: ")
+    #id = input("\nEnter a deck id (zero for random): ")
+    deck = DeckCreator(deck_name)
+    
+    print_commands()
+    ui = ""
+    while (ui == "" or ui[0] != "quit"):
+        ui = input("\nEnter a command: ").split()
+        if (ui[0] == "make_notes"):
+            if len(ui) != 4:
+                print("Expected 4 arguments, got", len(ui))
+                continue
+            # Determine model
+            if (ui[3] == "short_ans_model"):
+                model = short_ans_model
+            elif (ui[3] == "basic_model"):
+                model = basic_model
+            else:
+                print("Unknown model", ui[3])
+                continue
+            
+            if (ui[1] not in fp.get_titles()):
+                print("Unknown column", ui[1])
+                continue
+            if (ui[2] not in fp.get_titles()):
+                print("Unknown column", ui[2])
+                continue
+            
+            if (len(fp.data[ui[1]]) != len(fp.data[ui[2]])):
+                print("Error: could not pair columns, mismatched lengths")
+                continue
+            
+            for i in range(len(fp.data[ui[1]])):
+                deck.create_note((fp.data[ui[1]][i], fp.data[ui[2]][i]), model)
+            
+            print("Created {:d} notes in deck .".format(len(fp.data[ui[1]]))
+                                                        , deck_name)
+
+        elif (ui[0] == "write_out"):
+            if (len(ui) != 2):
+                print("Expected 2 arguments, received", len(ui))
+                continue
+            deck.write_to_file(ui[1])
+            print("Write out complete")
+
+        elif (ui[0] == "help"):
+            print_commands()
+        elif (ui[0] == "quit"):
+            continue
+        else:
+            print("Error: unknown command", ui[0])
+            continue
+            
